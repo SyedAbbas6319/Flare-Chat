acae1b2073c334f0356247ae21cdfb8c4b63b4d8
Syed Muhammad Abbas <syedabbas6319@gmail.com>
2024-09-17 02:05:30 -0700

    last

--- Frontend/src/components/messages/MessageInput.jsx
+++ Frontend/src/components/messages/MessageInput.jsx
@@ -25,7 +25,7 @@ const MessageInput = () => {
         />
         <button
           type="submit"
-          className="absolute inset-y-0 end-0 flex items-center pe-3"
+          className="absolute inset-y-0 end-0 flex items-center pe-3 "
         >
           {loading ? (
             <div className="loading loading-spinner"></div>
